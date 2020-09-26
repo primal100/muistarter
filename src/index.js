f9e37824810945f85cbc4fe8fef6e3195f91e821
Paul Martin <greatestloginnameever@gmail.com>
2020-09-26 20:07:11 +0100

    Remove Alerts.js

--- src/index.js
+++ src/index.js
@@ -21,7 +21,6 @@ export { default as AjaxForm } from './modules/form/AjaxForm'
 export { default as AjaxRequest } from './modules/components/AjaxRequest'
 export { default as Typography } from './modules/components/Typography'
 export { default as AppForm } from './modules/views/AppForm'
-export { default as Alerts } from './modules/Alerts'
 export { default as RFTextField } from './modules/form/RFTextField'
 export { default as AppBar} from './modules/views/AppAppBar';
 export { default as AppFooter } from './modules/views/AppFooter';
