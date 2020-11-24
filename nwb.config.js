132ad81f53aab2ee9daeb0a579b7b343f32fad9b
Paul Martin <greatestloginnameever@gmail.com>
2020-11-24 14:03:53 +0000

    SSR render buttons with proper colors

--- nwb.config.js
+++ nwb.config.js
@@ -12,5 +12,8 @@ module.exports = {
         }),
       ]
     }
+  },
+  babel: {
+    plugins: ['transform-regexp-constructors']
   }
 }
--- src/modules/form/FormButton.js
+++ src/modules/form/FormButton.js
@@ -2,9 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import defer from './defer';
 import Button from '../components/Button';
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const useStyles = makeStyles(theme => ({
+    notMounted: {
+      '&.Mui-disabled':{
+          backgroundColor: theme.palette.secondary.main,
+          color: theme.palette.secondary.light,
+        }
+      }
+    })
+)
+
 
 function FormButton(props) {
-  const { disabled, mounted, ...others } = props;
+  const classes = useStyles();
+  let { disabled, mounted, ...others } = props;
+  if (!mounted) others.classes = {disabled: classes.notMounted}
   return <Button disabled={!mounted || disabled} type="submit" variant="contained" {...others} />;
 }
 
